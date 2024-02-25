c29d614bd08b9a27867cc51539dabe95809d5833
Purity <118632852+wanjiruu-purity@users.noreply.github.com>
2024-02-25 09:55:40 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name=input("Enter your name")
+age = input("Enter your age")
+location = input("Enter your location")
+
+print("Hello" + " " + name + ", " + "you are" + " " + age + " " + "years old and live in" + " " + location + ".")
